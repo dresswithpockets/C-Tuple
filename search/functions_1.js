c45ee9329fd30d228316db046d2e83bed18bdb33
Tristen Horton <tristen@tristenhorton.com>
2016-06-28 21:38:56 -0600

    Add files via upload

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['make_5ftuple',['make_tuple',['../tuple_8h.html#a7c18a9b99b81dc06281da4ef9b07f21c',1,'tuple.c']]]
+  ['make_5ftuple',['make_tuple',['../tuple_8h.html#a73b2ab5636bd0c07072be9d5301da0ec',1,'tuple.c']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['tptr',['tptr',['../tuple_8h.html#aab8b4f7c3d6ed7f76939d132d9656f60',1,'tuple.c']]],
-  ['tptr_5fstack',['tptr_stack',['../tuple_8h.html#ad407b45f88fa00ec6e549b5b4f214acc',1,'tuple.c']]]
+  ['tptr_5fowned',['tptr_owned',['../tuple_8h.html#a686c6e334af297fc6fc23540cb13dfc4',1,'tuple.c']]],
+  ['tptr_5funowned',['tptr_unowned',['../tuple_8h.html#a27c87c74b11c1f626599143ab6f69859',1,'tuple.c']]]
 ];
