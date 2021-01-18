87cd6358cb7454e80e7f39c58a2432d0259c1630
Ashley <dresswithpockets@pm.me>
2021-01-18 06:21:16 -0700

    Update tuple.h

--- src/tuple.h
+++ src/tuple.h
@@ -1,6 +1,6 @@
 /*
 C-Tuple - Tuple style generic types in C.
-Copyright (c) 2016 Tristen Horton
+Copyright (c) 2020 dresswithpockets
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
